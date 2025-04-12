3637f8441e7a43265f0e2b4db9eeceff9e42e963
SumanBhandari7 <bhandarisuman51@gmail.com>
2025-04-12 11:09:53 +0545

    Completed

---
+++ script.js
@@ -0,0 +1,9 @@
+let gameNum=25;
+
+
+let userNum=prompt("Guess the Number:");
+while(userNum!=gameNum){
+    userNum=  prompt("You Guessed the Wrong Number,Guess Again:");
+
+}
+console.log("Congratulations, You entered the right number");
